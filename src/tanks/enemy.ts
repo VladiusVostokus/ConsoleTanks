f5b6f2d8febf3190a6bd98c0b6be224f33cc28c5
Kiril Yakymchuk <stbestich@gmail.com>
2024-06-18 12:07:14 +0300

    craeted enemy class

---
+++ src/tanks/enemy.ts
@@ -0,0 +1,12 @@
+import { ITank } from "./ITank";
+
+export class Enemy extends ITank {
+  constructor(coordinates: { x: number, y: number }) {
+    const health = Math.floor(Math.random() * 3) + 1;
+    const speed = Math.floor(Math.random() * 3) + 1;
+    const symbol = '*';
+    super(coordinates, health, speed, symbol);
+  }
+
+  act(gameField: unknown) {}
+}
