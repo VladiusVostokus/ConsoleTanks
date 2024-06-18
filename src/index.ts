044cbd4c9591cf36ac85d839e3ed775e419d9a32
VladiusVostokus <kovalenkov222@gmail.com>
2024-06-18 17:31:40 +0300

    constans intead of magic numbers

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
 import { GameField } from './gamefield';
 
-const gameField = new GameField(' ', 10, 10)
+const HEIGHT: number = 20;
+const WIDTH: number = 20;
+const EMPTY_SPACE: string = ' ';
+
+const gameField = new GameField(EMPTY_SPACE, WIDTH, HEIGHT);
 gameField.updateGameField();
 gameField.showGameField();
 
