d11470100e249c974ba8aa14f62e1acce239ccfb
Kiril Yakymchuk <stbestich@gmail.com>
2024-06-18 12:09:55 +0300

    created projectile class

---
+++ src/projectile/projectile.ts
@@ -0,0 +1,17 @@
+export class Projectile {
+  x: number;
+  y: number;
+  symbol: string;
+  speed: number;
+  direction: number;
+
+  constructor(coordinates: { x: number, y: number }, direction: number, speed: number, symbol: string) {
+    this.x = coordinates.x;
+    this.y = coordinates.y;
+    this.direction = direction;
+    this.speed = speed;
+    this.symbol = symbol;
+  }
+
+  move() {}
+}
