b73e6a41417e2918b0884bbc7f94d1d732ce97dc
VladiusVostokus <kovalenkov222@gmail.com>
2024-06-18 15:25:45 +0300

    Add test for gamefiedl wall and space value
    Check if wall cells are equal to '#' after creating the gamefield and
    check if cells inside equal to ' ' after creating, so walls and space
    created correctly.

--- test/gamefield.spec.ts
+++ test/gamefield.spec.ts
@@ -19,5 +19,28 @@ describe('Check properties of gamefiedl', () => {
         const cell:string = gameField.getCell(5,5);
         expect(cell).toBe('%');
     });
+
+    it('Check wall and space values', () => {
+        gameField.updateGameField();
+        const topWall = gameField.getCell(0,3);
+        const rightWall = gameField.getCell(5,9);
+        const bottomWall = gameField.getCell(9,4);
+        const leftWall = gameField.getCell(6,0);
+
+        expect(topWall).toBe('#');
+        expect(rightWall).toBe('#');
+        expect(bottomWall).toBe('#');
+        expect(leftWall).toBe('#');
+
+        const upperLeftSpace = gameField.getCell(1,1);
+        const upperRightSpace = gameField.getCell(1,8);
+        const lowerRightSpace = gameField.getCell(8,8);
+        const loverLeftSpace = gameField.getCell(8,1);
+
+        expect(upperLeftSpace).toBe(' ');
+        expect(upperRightSpace).toBe(' ');
+        expect(lowerRightSpace).toBe(' ');
+        expect(loverLeftSpace).toBe(' ');
+    });
 });
   
