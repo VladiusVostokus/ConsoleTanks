25ce145c6583577e3865b2e5dfd84eba6344b9fc
VladiusVostokus <kovalenkov222@gmail.com>
2024-06-19 15:17:32 +0300

    Put all positions of tank on gamefield
    Add 4 methods to put tank on gamefield with some of 4 positions to see
    how it looks like and to use them in the future for movements of tank.

--- src/index.ts
+++ src/index.ts
@@ -5,8 +5,14 @@ const HEIGHT: number = 20;
 const WIDTH: number = 20;
 
 const gameField = new GameField(WIDTH, HEIGHT);
-const tank = new Tank(5,5);
+const tank1 = new Tank(3,3);
+const tank2 = new Tank(3,10);
+const tank3 = new Tank(10,10);
+const tank4 = new Tank(10,3);
 gameField.updateGameField();
-tank.putTank(gameField);
+tank1.putTankUp(gameField);
+tank2.putTankDown(gameField);
+tank3.putTankLeft(gameField);
+tank4.putTankRight(gameField);
 gameField.showGameField();
 
--- src/tank.ts
+++ src/tank.ts
@@ -7,16 +7,53 @@ export class Tank{
                                   ['@','#','@'],
                                   ['@','#','@'],
                                 ];
+    private _downPos: string[][] = [['@','#','@'],
+                                    ['@','#','@'],
+                                    [' ','#',' '],
+                                ];
+    private _leftPos: string[][] = [[' ','@','@'],
+                                    ['#','#','#'],
+                                    [' ','@','@'],
+                                ];
+    private _rightPos: string[][] = [['@','@',' '],
+                                     ['#','#','#'],
+                                     ['@','@',' '],
+                                ];
 
-    constructor(x: number, y:number) {
-        this._x = x;
+
+    constructor(y: number, x:number) {
         this._y = y;
+        this._x = x;
     }
 
-    putTank(gamefield: GameField) {
+    putTankUp(gamefield: GameField) {
         for (let i:number = 0; i < 3; i++){
             for (let j:number = 0; j < 3; j++) {
                 gamefield.setCell(this._y+i,this._x+j,this._upPos[i][j]);
+            }
+        }
+    }
+
+    putTankDown(gamefield: GameField) {
+        for (let i:number = 0; i < 3; i++){
+            for (let j:number = 0; j < 3; j++) {
+                gamefield.setCell(this._y+i,this._x+j,this._downPos[i][j]);
+            }
+        }
+    }
+
+    putTankLeft(gamefield: GameField) {
+        for (let i:number = 0; i < 3; i++){
+            for (let j:number = 0; j < 3; j++) {
+                gamefield.setCell(this._y+i,this._x+j,this._leftPos[i][j]);
+            }
+        }
+    }
+
+    putTankRight(gamefield: GameField) {
+        for (let i:number = 0; i < 3; i++){
+            for (let j:number = 0; j < 3; j++) {
+                gamefield.setCell(this._y+i,this._x+j,this._rightPos[i][j]);
             }
         }
     }
