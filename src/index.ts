34a9e707edef72968f02ab06491f7495b7cb57d0
Kiril Yakymchuk <stbestich@gmail.com>
2024-06-21 15:38:36 +0300

    difficulty level

--- src/index.ts
+++ src/index.ts
@@ -8,8 +8,8 @@ import { options } from '@stlib/utils';
 const HEIGHT: number = Number(options?.size ?? 20);
 const WIDTH: number = Number(options?.size ?? 20);
 const GAME_UPDATE_TIMEOUT: number = 60;
-const ENEMY_MOVE_TIMEOUT: number = 500;
-const ENEMY_FIRE_TIMEOUT: number = 1000;
+const ENEMY_MOVE_TIMEOUT: number = 500 / Number(options?.difficulty || 1);
+const ENEMY_FIRE_TIMEOUT: number = 1000 / Number(options?.difficulty || 1);
 
 const gameField = new GameField(WIDTH, HEIGHT);
 const player = new Player(9, 9, WIDTH, HEIGHT);
