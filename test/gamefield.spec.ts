56f7e481969b1b8119f6940301dea49ed9ee8dbc
Kiril Yakymchuk <stbestich@gmail.com>
2024-06-21 12:51:43 +0300

    refactored tests

--- test/gamefield.spec.ts
+++ test/gamefield.spec.ts
@@ -1,55 +1,55 @@
 import { GameField } from '../src/gamefield';
 
 describe('Gamefield class', () => {
-    const HEIGHT: number = 10;
-    const WIDTH: number = 10;
-    let gameField: GameField;
+  const HEIGHT: number = 10;
+  const WIDTH: number = 10;
+  let gameField: GameField;
 
-    beforeEach(() => {
-      gameField = new GameField(HEIGHT, WIDTH);
-    })
+  beforeEach(() => {
+    gameField = new GameField(HEIGHT, WIDTH);
+  });
 
-    it('Check size', () => {
-        gameField.updateGameField();
-        expect(gameField.getHeight()).toBe(HEIGHT);
-        expect(gameField.getWidth()).toBe(WIDTH);
+  it('Check size', () => {
+    gameField.updateGameField();
+    expect(gameField.getHeight()).toBe(HEIGHT);
+    expect(gameField.getWidth()).toBe(WIDTH);
 
-        gameField.updateGameField();
-        expect(gameField.getHeight()).toBe(HEIGHT);
-        expect(gameField.getWidth()).toBe(WIDTH);
-    });
+    gameField.updateGameField();
+    expect(gameField.getHeight()).toBe(HEIGHT);
+    expect(gameField.getWidth()).toBe(WIDTH);
+  });
 
-    it('Check get and set', () => {
-        gameField.updateGameField();
-        gameField.setCell(5, 5,'%');
-        const cell:string = gameField.getCell(5,5);
-        expect(cell).toBe('%');
-    });
+  it('Check get and set', () => {
+    gameField.updateGameField();
+    gameField.setCell(5, 5, '%');
+    const cell: string = gameField.getCell(5, 5);
+    expect(cell).toBe('%');
+  });
 
-    it('Check wall and space values', () => {
-        gameField.updateGameField();
-        const topWall = gameField.getCell(0,3);
-        const rightWall = gameField.getCell(5,9);
-        const bottomWall = gameField.getCell(9,4);
-        const leftWall = gameField.getCell(6,0);
+  it('Check wall and space values', () => {
+    gameField.updateGameField();
+    const topWall = gameField.getCell(0, 3);
+    const rightWall = gameField.getCell(5, 9);
+    const bottomWall = gameField.getCell(9, 4);
+    const leftWall = gameField.getCell(6, 0);
 
-        const wall = gameField.wall;
+    const wall = gameField.wall;
 
-        expect(topWall).toBe(wall);
-        expect(rightWall).toBe(wall);
-        expect(bottomWall).toBe(wall);
-        expect(leftWall).toBe(wall);
+    expect(topWall).toBe(wall);
+    expect(rightWall).toBe(wall);
+    expect(bottomWall).toBe(wall);
+    expect(leftWall).toBe(wall);
 
-        const upperLeftSpace = gameField.getCell(1,1);
-        const upperRightSpace = gameField.getCell(1,8);
-        const lowerRightSpace = gameField.getCell(8,8);
-        const loverLeftSpace = gameField.getCell(8,1);
+    const upperLeftSpace = gameField.getCell(1, 1);
+    const upperRightSpace = gameField.getCell(1, 8);
+    const lowerRightSpace = gameField.getCell(8, 8);
+    const loverLeftSpace = gameField.getCell(8, 1);
 
-        const space = gameField.space;
+    const space = gameField.space;
 
-        expect(upperLeftSpace).toBe(space);
-        expect(upperRightSpace).toBe(space);
-        expect(lowerRightSpace).toBe(space);
-        expect(loverLeftSpace).toBe(space);
-    });
+    expect(upperLeftSpace).toBe(space);
+    expect(upperRightSpace).toBe(space);
+    expect(lowerRightSpace).toBe(space);
+    expect(loverLeftSpace).toBe(space);
+  });
 });
--- test/player.spec.ts
+++ test/player.spec.ts
@@ -1,32 +1,32 @@
 import { Player } from '../src/player';
 
-jest.mock('../src/tank'); 
+jest.mock('../src/tank');
 
 describe('Player class', () => {
   let player: Player;
 
   beforeEach(() => {
-    player = new Player(5, 5, 20, 20)
-  })
+    player = new Player(5, 5, 20, 20);
+  });
 
-    it('Check correct call control functions', () => {
-        player.action('w');
-        expect(player.turnUp).toHaveBeenCalledTimes(1);
+  it('Check correct call control functions', () => {
+    player.action('w');
+    expect(player.turnUp).toHaveBeenCalledTimes(1);
 
-        player.action('s');
-        expect(player.turnDown).toHaveBeenCalledTimes(1);
+    player.action('s');
+    expect(player.turnDown).toHaveBeenCalledTimes(1);
 
-        player.action('a');
-        expect(player.turnLeft).toHaveBeenCalledTimes(1);
+    player.action('a');
+    expect(player.turnLeft).toHaveBeenCalledTimes(1);
 
-        player.action('d');
-        expect(player.turnRight).toHaveBeenCalledTimes(1);
+    player.action('d');
+    expect(player.turnRight).toHaveBeenCalledTimes(1);
 
-        player.action('f');
-        expect(player.fire).toHaveBeenCalledTimes(1);
-    });
+    player.action('f');
+    expect(player.fire).toHaveBeenCalledTimes(1);
+  });
 
-    it('Check correct hanfling of unknown button', () => {
-        expect(() => player.action('z')).not.toThrow();
-    });
+  it('Check correct hanfling of unknown button', () => {
+    expect(() => player.action('z')).not.toThrow();
+  });
 });
