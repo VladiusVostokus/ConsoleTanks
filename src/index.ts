9a74d2bf36b9284dc961968cbd05eb2f1d7eb0a0
Kiril Yakymchuk <stbestich@gmail.com>
2024-06-21 15:45:01 +0300

    spawn new enemies

--- src/index.ts
+++ src/index.ts
@@ -11,9 +11,9 @@ const GAME_UPDATE_TIMEOUT: number = 60;
 const ENEMY_MOVE_TIMEOUT: number = 500 / Number(options?.difficulty || 1);
 const ENEMY_FIRE_TIMEOUT: number = 1000 / Number(options?.difficulty || 1);
 
-const gameField = new GameField(WIDTH, HEIGHT);
-const player = new Player(9, 9, WIDTH, HEIGHT);
-const enemy = new Enemy(5, 5, WIDTH, HEIGHT);
+const gameField: GameField = new GameField(WIDTH, HEIGHT);
+const player: Player = new Player(9, 9, WIDTH, HEIGHT);
+const enemies: Enemy[] = [new Enemy(5, 5, WIDTH, HEIGHT)];
 
 readline.emitKeypressEvents(process.stdin);
 if (process.stdin.isTTY) {
@@ -24,9 +24,11 @@ const updateGame = () => {
   console.clear();
   gameField.updateGameField();
   player.putTank(gameField);
-  enemy.putTank(gameField);
   player.updateProjectiles(gameField);
-  enemy.updateProjectiles(gameField);
+  enemies.forEach(enemy => {
+    enemy.putTank(gameField);
+    enemy.updateProjectiles(gameField);
+  })
   gameField.showGameField();
 };
 
@@ -35,5 +37,29 @@ process.stdin.on('keypress', (button: string) => {
 });
 
 setInterval(updateGame, GAME_UPDATE_TIMEOUT);
-setInterval(() => enemy.moveRandomly(), ENEMY_MOVE_TIMEOUT);
-setInterval(() => enemy.fireRandomly(), ENEMY_FIRE_TIMEOUT);
+setInterval(() => enemies.forEach(enemy => {
+  enemy.moveRandomly();
+}), ENEMY_MOVE_TIMEOUT);
+setInterval(() => enemies.forEach(enemy => {
+  enemy.fireRandomly();
+}), ENEMY_FIRE_TIMEOUT);
+
+const spawnEnemy = () => {
+  if (enemies.length < 3) {
+    const randomX = Math.floor(Math.random() * (WIDTH - 4)) + 1;
+    const randomY = Math.floor(Math.random() * (HEIGHT - 4)) + 1;
+    const newEnemy = new Enemy(randomY, randomX, WIDTH, HEIGHT);
+    enemies.push(newEnemy);
+  }
+};
+
+const randomSpawnTime = () => Math.random() * 5000 + 5000;
+
+const spawnEnemyInterval = () => {
+  setTimeout(() => {
+    spawnEnemy();
+    spawnEnemyInterval();
+  }, randomSpawnTime());
+};
+
+spawnEnemyInterval();
